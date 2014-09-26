cd5ccd8eb4c49468f27b5d7a08f7861bca12697c
unknown <nirvajara@hotmail.com>
2014-09-26 17:24:11 -0430

    heroku 2

--- domiclik/settings.py
+++ domiclik/settings.py
@@ -1,6 +1,6 @@
 # Django settings for domiclik project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 
@@ -67,7 +67,7 @@ MEDIA_URL = 'http://domiclik.herokuapp.com/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = RUTA_PROYECTO.child('static')
+STATIC_ROOT = RUTA_PROYECTO.child('static')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
@@ -79,7 +79,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    RUTA_PROYECTO.child('static'),
+    #RUTA_PROYECTO.child('static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
