e4d23160e069ae2795f3119a6a365edf29d4fe8a
Andrade <nirvajara@hotmail.com>
2014-11-15 10:26:49 -0430

    staticos

--- domiclik/settings.py
+++ domiclik/settings.py
@@ -78,6 +78,7 @@ MEDIA_URL = 'http://domiclik.herokuapp.com/media/'
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = RUTA_PROYECTO.child('static')
 
+
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 
@@ -88,7 +89,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # RUTA_PROYECTO.child('static'),
+    #RUTA_PROYECTO.child('static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
